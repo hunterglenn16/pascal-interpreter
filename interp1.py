d238c968e58ee141f9d3155250762d64099b1c5f
hunterglenn16 <hunterglenn16@gmail.com>
2025-05-17 22:42:04 -0400

    Updated test program for pascal

--- interp1.py
+++ interp1.py
@@ -482,9 +482,16 @@ class Interpreter(NodeVisitor):
 
 def main():
     text = """\
-        BEGIN
-            x :=1
-        END."""
+        PROGRAM Part10AST;
+        VAR
+            a, b : INTEGER;
+            y    : REAL;
+
+        BEGIN {Part10AST}
+            a := 2;
+            b := 10 * a + 10 * a DIV 4;
+            y := 20 / 7 + 3.14;
+        END.  {Part10AST}"""
     lexer = Lexer(text)
     parser = Parser(lexer)
     interpreter = Interpreter(parser)
